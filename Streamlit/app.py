f5fd73ba6d1e7fbb65dab699c0f05543c410873d
Ana Pinheiro <ana.pinheiro@eaware.io>
2025-05-22 22:14:33 -0300

    criação da pasta de desenvolvimento para um novo projeto

---
+++ Streamlit/app.py
@@ -0,0 +1,9 @@
+# pip install streamlit
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# https://www.kaggle.com/datasets/radheshyamkollipara/bank-customer-churn/data
+
+st.title("Análise de Dados - Exemplo")
